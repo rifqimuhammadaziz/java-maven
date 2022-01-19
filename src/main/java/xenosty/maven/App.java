fad966cef10950f9c6be1ff42e21d4b80d73d414
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-19 14:57:54 +0700

    add Gson library & create json app

--- src/main/java/xenosty/maven/App.java
+++ src/main/java/xenosty/maven/App.java
@@ -1,5 +1,7 @@
 package xenosty.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Gson gson = new Gson();
+
+        Person person = new Person("Xenosty");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
